40b6ad9dcad8790189c5ee5a916c2ddd9ce66691
Anupam kumar Singh <anupampom@gmail.com>
2016-09-07 22:30:03 +0530

    Update mytest.R

--- R/mytest.R
+++ R/mytest.R
@@ -2,7 +2,7 @@
 myfirstpack <- function(x,y)
   {
 g = t.test(x,y)
-h = list("The means are " ,g$estimate,"The upper and lower bound of confidence intervals are ", g$conf.int,"The p value is ",g$p.value)
+h = c("The means are " ,g$estimate,"The upper and lower bound of confidence intervals are ", g$conf.int,"The p value is ",g$p.value)
 h[4]
 }
 
