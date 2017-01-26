dd58470b711409f8332216a345694d7415bfa553
Anupam kumar Singh <anupampom@gmail.com>
2017-01-26 21:21:19 +0530

    sd
    sd

--- R/mytest.R
+++ R/mytest.R
@@ -2,7 +2,9 @@
 myfirstpack <- function(x,y)
   {
 g = t.test(x,y)
-h = c("The means  are " ,g$estimate,"The standard deviation of group X is" ,sd(x), "The standard deviation of Y is",sd(y)  ,"The upper and lower bound of confidence intervals are ", g$conf.int,"The p value is ",g$p.value)
+a = sd(x)
+b = sd(y)
+h = c("The means  are " ,g$estimate,"The standard deviation of group X is" ,a, "The standard deviation of Y is",b ,"The upper and lower bound of confidence intervals are ", g$conf.int,"The p value is ",g$p.value)
 return(h)
 }
 
