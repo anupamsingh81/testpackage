71fae2a90c9a3d832b94b522d844a415646a6510
Anupam kumar Singh <anupampom@gmail.com>
2017-05-02 12:11:19 +0530

    Update mytest.R
    retrodesign

--- R/mytest.R
+++ R/mytest.R
@@ -51,3 +51,16 @@ Bfkay<-function(sd, obtained, uniform, lower=0, upper=1, meanoftheory=0, sdtheor
 }
 
 
+retrodesign <- function(A, s, alpha=.05, df=Inf, n.sims=10000){
+z <- qt(1-alpha/2, df) 
+p.hi <- 1 - pt(z-A/s, df) 
+p.lo <- pt(-z-A/s, df)  
+power <- p.hi + p.lo
+typeS <- p.lo/power
+estimate <- A + s*rt(n.sims,df)
+significant <- abs(estimate) > s*z
+exaggeration <- mean(abs(estimate)[significant])/A
+return(list(power=power, typeS=typeS, exaggeration=exaggeration))
+}
+
+
